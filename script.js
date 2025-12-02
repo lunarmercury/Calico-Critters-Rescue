903ce70645b2aa1900f92ad645245af0b751ec60
lunarmercury <lunarmercury0411@gmail.com>
2025-12-02 13:42:47 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'pastel-pink': '#fbcfe8', 
+                'pastel-green': '#a7f3d0',
+                'pastel-blue': '#bfdbfe', 
+                'pastel-yellow': '#fde68a', 
+                'pastel-light': '#fff7ed', 
+            },
+        }
+    }
+}
